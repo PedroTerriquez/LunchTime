2509cb8973be37e8cc98026b62a5dd26e047c404
Pedro Terriquez <pedroterriquez@hotmail.com>
2018-09-10 10:27:46 -0500

    Procfile for Heroku

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 const foods = ['Pollito chiken, gallina hen','Ensaladita, ponte sano bb','Dick']
 
 app.get('/food/today', (req, res) => res.send(foods[Math.floor(Math.random()*foods.length)]))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
