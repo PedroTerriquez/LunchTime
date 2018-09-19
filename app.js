c9cce067501cf958cf966f8addcabcf3dc5c04d6
Pedro Terriquez <pedroterriquez@hotmail.com>
2018-09-19 12:19:44 -0500

    Adding CORS to API

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const Compression = require('compression')
 const BodyParser = require('body-parser')
 const Logger = require('morgan')
 const Helmet = require('helmet')
+const cors = require('cors')
+
+app.use(cors())
 
 // Security
 app.use(Helmet())
@@ -15,14 +18,6 @@ app.use(Compression())
 
 // Debug
 app.use(Logger('dev'))
-
-app.use((req, res, next) => {
- 	response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Credentials", "true");
-  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
 
 // Request params
 app.use(BodyParser.json())
