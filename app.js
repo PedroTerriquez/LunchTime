899f853a6a0794dd95f95fbb53d4e170505c50da
Pedro Terriquez <pedroterriquez@hotmail.com>
2018-09-19 11:43:35 -0500

    Adding CORS to API

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(Compression())
 // Debug
 app.use(Logger('dev'))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Request params
 app.use(BodyParser.json())
 app.use(BodyParser.urlencoded({ extended: true }))
