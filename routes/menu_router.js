b32452202fed348ae4fe5690080ab9d378038fc7
Pedro Terriquez <pedroterriquez@hotmail.com>
2018-09-24 14:47:29 -0500

    Adding weekly and monthly service

--- routes/menu_router.js
+++ routes/menu_router.js
@@ -6,14 +6,51 @@ const menuRouter = Express.Router();
 const permittedParams = ['date', 'dishes'];
 
 menuRouter.get('', (req, res) => {
-	Menu.find({}, (err, menues) => {
+	Menu
+		.find({})
+		.populate('dishes')
+		.exec((err, menues) => {
 		res.send(menues);
+	})
+});
+
+menuRouter.get('/week', (req, res) => {
+	range = range_for_this_week();
+	console.log(range)
+	Menu
+		.find({
+			date: {
+				$gte: range[0],
+				$lt: range[1]
+			}
+		})
+		.populate('dishes')
+		.exec((err, menu) => {
+		res.json(menu);
+	})
+});
+
+menuRouter.get('/month', (req, res) => {
+	range = range_for_this_month();
+	Menu
+		.find({
+			date: {
+				$gte: range[0],
+				$lt: range[1]
+			}
+		})
+		.populate('dishes')
+		.exec((err, menu) => {
+		res.json(menu);
 	})
 });
 
 menuRouter.get('/day/:date', (req, res) => {
 	date = new Date(req.params.date).toISOString()
-	Menu.findOne({date: date}).populate('dishes').exec((err, menu) => {
+	Menu
+		.findOne({date: date})
+		.populate('dishes')
+		.exec((err, menu) => {
 		res.json(menu);
 	})
 });
@@ -36,8 +73,28 @@ menuRouter.delete('/:id', (req, res) => {
 		res.status(204).json(menu)
 	})
 })
+
 function menu_params(body){
 	return params(body).only(permittedParams)
+}
+
+function range_for_this_month(){
+	date = new Date();
+	year = date.getFullYear();
+	month = date.getMonth();
+	firstDay = new Date(year, month, 1).toISOString();
+	lastDay = new Date(year, month+1, 0).toISOString();
+	return [firstDay, lastDay]
+}
+
+function range_for_this_week(){
+  d = new Date()
+  var day = d.getDay()
+  var diff = d.getDate() - day + (day == 0 ? -6:1);
+  date = new Date(d.setDate(diff));
+  monday = new Date(date.getFullYear(), date.getMonth(), date.getDate()).setUTCHours(0,0,0,0)
+  friday = new Date(d.setDate(diff + 5));
+  return [monday, friday]
 }
 
 module.exports = menuRouter;
