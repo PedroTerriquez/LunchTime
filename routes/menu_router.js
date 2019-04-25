f7e9d89f3eb5ed9e44b7aebb3ae5e700176a09f1
Pedro Terriquez <pedroterriquez@hotmail.com>
2019-04-25 16:40:46 -0500

    Fixing Swipe API

--- routes/menu_router.js
+++ routes/menu_router.js
@@ -6,83 +6,80 @@ const menuRouter = Express.Router();
 const permittedParams = ['date', 'dishes'];
 
 menuRouter.get('', (req, res) => {
-	Menu
-		.find({})
-		.populate('dishes')
-		.exec((err, menues) => {
-		res.send(menues);
-	})
+  Menu
+    .find({})
+    .populate('dishes')
+    .exec((err, menues) => {
+      res.send(menues);
+    })
 });
 
 menuRouter.get('/week', (req, res) => {
-	range = range_for_this_week();
-	console.log(range)
-	Menu
-		.find({
-			date: {
-				$gte: range[0],
-				$lt: range[1]
-			}
-		})
-		.populate('dishes')
-		.exec((err, menu) => {
-		res.json(menu);
-	})
+  range = range_for_this_week();
+  console.log(range)
+  Menu
+    .find({
+      date: {
+        $gte: range[0],
+        $lt: range[1]
+      }
+    })
+    .populate('dishes')
+    .exec((err, menu) => {
+      res.json(menu);
+    })
 });
 
 menuRouter.get('/month', (req, res) => {
-	range = range_for_this_month();
-	Menu
-		.find({
-			date: {
-				$gte: range[0],
-				$lt: range[1]
-			}
-		})
-		.populate('dishes')
-		.exec((err, menu) => {
-		res.json(menu);
-	})
+  range = range_for_this_month();
+  Menu
+    .find({
+      date: {
+        $gte: range[0],
+        $lt: range[1]
+      }
+    })
+    .populate('dishes')
+    .exec((err, menu) => {
+      res.json(menu);
+    })
 });
 
 menuRouter.get('/day/:date', (req, res) => {
-	date = new Date(req.params.date).toISOString()
-	Menu
-		.findOne({date: date})
-		.populate('dishes')
-		.exec((err, menu) => {
-		res.json(menu);
-	})
+  date = new Date(req.params.date).toISOString()
+  Menu
+    .findOne({date: date})
+    .populate('dishes')
+    .exec((err, menu) => {
+      res.json(menu);
+    })
 });
 
 menuRouter.post('', (req, res) => {
-	var menu = new Menu(menu_params(req.body))
-	menu.save((err, menu) => {
-		res.status(201).json(menu);
-	})
+  var menu = new Menu(menu_params(req.body))
+  menu.save((err, menu) => {
+    res.status(201).json(menu);
+  })
 });
 
 menuRouter.patch('/switch/:id', (req, res) => {
-	const id = req.params.id
-	const params = req.body
-	Menu.findOne({ _id: id }).exec((err, menu) => {
-		if (menu) {
-			Menu.findOneAndUpdate({ date: params.date }, { date: null }).exec((err, menuTarget) => {
-				let tempDate = menu.date
-				menu.set({ date: params.date })
-				menu.save((err, updatedMenu) => {
-					if (menuTarget) {
-						menuTarget.date = tempDate
-						menuTarget.save((err, updatedTargetMenu) => {
-							return res.status(201).json(menu);
-						})
-					} else {
-						return res.status(201).json(menu);
-					}
-				})
-			})
-		}
-	})
+  const id = req.params.id
+  const params = req.body
+  Menu.findOne({ _id: id }).exec((err, menu) => {
+    if (menu) {
+      Menu.findOneAndUpdate({ _id: params.idTo }, { date: null }).exec((err, menuTarget) => {
+        if(menuTarget){
+          menuTarget.date = menu.date
+          menu.date = params.date
+          menu.save((err1, updatedMenu) => {
+            menuTarget.save((err2, updatedTargetMenu) => {
+              return res.status(201).json(menu);
+            })
+          })
+        }
+      })
+    }
+  })
 })
 
 menuRouter.patch('/pushDish/:id', (req, res) => {
@@ -99,28 +96,28 @@ menuRouter.patch('/pullDish/:id', (req, res) => {
 
 
 menuRouter.patch('/:id', (req, res) => {
-	Menu.findOneAndUpdate({_id: req.params.id}, menu_params(req.body) ,(err, menu) => {
-		res.json(menu)
-	})
+  Menu.findOneAndUpdate({_id: req.params.id}, menu_params(req.body) ,(err, menu) => {
+    res.json(menu)
+  })
 })
 
 menuRouter.delete('/:id', (req, res) => {
-	Menu.findOneAndRemove({_id: req.params.id}, (err, menu) => {
-		res.status(204).json(menu)
-	})
+  Menu.findOneAndRemove({_id: req.params.id}, (err, menu) => {
+    res.status(204).json(menu)
+  })
 })
 
 function menu_params(body){
-	return params(body).only(permittedParams)
+  return params(body).only(permittedParams)
 }
 
 function range_for_this_month(){
-	date = new Date();
-	year = date.getFullYear();
-	month = date.getMonth();
-	firstDay = new Date(year, month, 1).toISOString();
-	lastDay = new Date(year, month+1, 0).toISOString();
-	return [firstDay, lastDay]
+  date = new Date();
+  year = date.getFullYear();
+  month = date.getMonth();
+  firstDay = new Date(year, month, 1).toISOString();
+  lastDay = new Date(year, month+1, 0).toISOString();
+  return [firstDay, lastDay]
 }
 
 function range_for_this_week(){
